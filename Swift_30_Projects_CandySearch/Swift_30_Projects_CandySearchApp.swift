b49fdeca2e171087decd9bc00cb01ee6a909691e
prettyturtle <unifconti@gmail.com>
2023-05-06 23:56:38 +0900

    Initial Commit

---
+++ Swift_30_Projects_CandySearch/Swift_30_Projects_CandySearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_30_Projects_CandySearchApp.swift
+//  Swift_30_Projects_CandySearch
+//
+//  Created by yc on 2023/05/06.
+//
+
+import SwiftUI
+
+@main
+struct Swift_30_Projects_CandySearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
